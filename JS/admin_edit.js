13c04b4e6bd0931f74b06c3074fbf18427c4641b
hasibulalamrahi <hasibulalam6919@gmail.com>
2021-08-19 19:37:11 +0600

    latest commits are added here

--- JS/admin_edit.js
+++ JS/admin_edit.js
@@ -16,18 +16,18 @@ class Book {
     //     window.open("Admin_Login.html", "_self");
     //  }
       const books = Store.getBooks();
-      console.log(books);
+      // console.log(books);
       books.forEach((book) => UI.addBookToList(book));
     }
-  
     static addBookToList(book) {
-      const list = document.querySelector('#book-list');
+      // console.log(book);
+      let list = document.querySelector('#book-list');
       let url = `https://books.google.com.bd/books?vid=ISBN`;
       let name = book.isbn;
       url+=name;
       let chk = '<a href ="'+ url+'">'+name+'</a>'
       // let imgTag = '<img src =" '+imgUrl'" alt  '
-      const row = document.createElement('tr');
+      let row = document.createElement('tr');
       
   
       row.innerHTML = `
@@ -35,6 +35,7 @@ class Book {
         <td>${book.author}</td>
         <td>${chk}</td>
         <td>${book.img}</td>
+        <td><a href="#" class="btn btn-dark dark">EDIT</a></td>
         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
       `;
   
@@ -45,8 +46,30 @@ class Book {
       if(el.classList.contains('delete')) {
         el.parentElement.parentElement.remove();
       }
+      
     }
-  
+     static editBook(el){
+      if(el.classList.contains('dark')) {
+      var isbnEdit =el.parentElement.previousElementSibling.previousElementSibling.textContent;
+      // console.log(isbnEdit);
+      Store.startEdit(isbnEdit);
+      }
+     }   
+     static showEditedBooks(BookDetails) {
+      //  console.log(BookDetails);
+        //  let a  = document.querySelector('#book-form');
+
+        
+        document.querySelector('#title').value = BookDetails.title ;
+        document.querySelector('#author').value =BookDetails.author ;
+        document.querySelector('#isbn').value = BookDetails.isbn ;
+        
+            
+            
+        };
+
+        
+     
     static showAlert(message, className) {
       const div = document.createElement('div');
       div.className = `alert alert-${className}`;
@@ -92,17 +115,35 @@ class Book {
   
     static removeBook(isbn) {
       const books = Store.getBooks();
-      alert("Do You Want to remove this Book");
+      // alert("Do You Want to remove this Book");
       books.forEach((book, index) => {
         // console.log(index);
         if(book.isbn === isbn) {
           console.log(index);
 
           books.splice(index, 1);
+          UI.showAlert('Book Removed', 'success');
+
         }
       });
   
       localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static startEdit(isbnEdit){
+        // console.log(isbnEdit);
+        const storedBooks = JSON.parse(localStorage.getItem('books'));
+        // console.log(storedBooks.length);
+        for(var i = 0 ; i<(storedBooks.length );i++){
+         if(storedBooks[i].isbn == isbnEdit){
+            // console.log(storedBooks[i]);
+            UI.showEditedBooks(storedBooks[i]);
+            
+          }
+      }
+
+        // UI.showAlert('Book Edited','success');
+
     }
   }
   
@@ -165,10 +206,13 @@ class Book {
     UI.deleteBook(e.target);
   
     // Remove book from store
-    Store.removeBook(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
-    // console.log(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+
+    Store.removeBook(e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent);
+    // console.log(e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent);
     // Show success message
-    UI.showAlert('Book Removed', 'success');
+    // UI.showAlert('Book Removed', 'success');
+    UI.editBook(e.target);
+    // console.log(e.target);
   });
   
 
